e1e274ce78f51e8bc95fe017a46788983be8c840
Vova iz Tambova <voller.ruden@yandex.ru>
2023-10-13 14:56:13 +0300

    Реализуйте логирование

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
+app.use(requestLogger);
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(router);
+
+app.use(errorLogger);
 app.use(errors());
 
 app.listen(3000);
