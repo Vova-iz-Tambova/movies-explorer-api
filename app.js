2b7d4fe75374c2ee5c56e97ca3b0527c8c6b18d4
Vova iz Tambova <voller.ruden@yandex.ru>
2023-11-18 09:41:38 +0300

    fix validate

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { apiLimiter } = require('./middlewares/limiter');
+// const { apiLimiter } = require('./middlewares/limiter');
 
 const { NODE_ENV, MONGO_DB } = process.env;
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(requestLogger);
 app.use(express.json());
 app.use(helmet());
-app.use(apiLimiter);
+// app.use(apiLimiter);
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : 'mongodb://127.0.0.1:27017/bitfilmsdb');
 
